cc8c59aa8829a50a8819b2c9bd0bbcb931fa3140
Kevin Tran <ktran@tack.tech>
2018-06-05 22:25:14 -0700

    add current location to map

--- app/src/main/java/edu/washington/zhang007/foodfighters/MapsActivity.kt
+++ app/src/main/java/edu/washington/zhang007/foodfighters/MapsActivity.kt
@@ -1,26 +1,127 @@
 package edu.washington.zhang007.foodfighters
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private var TAG = "MainActivity"
     private lateinit var mMap: GoogleMap
+
+    private var latitude:Double=0.toDouble()
+    private var longitude:Double=0.toDouble()
+
+    private lateinit var mLastLocation:Location
+    private var mMarker: Marker?=null
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+
+    companion object {
+        private const val MY_PERMISSION_CODE: Int = 1000
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        // Request runtime permission
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission()
+        }
+        buildLocationRequest()
+        buildLocationCallback()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+    }
+
+    private fun buildLocationCallback() {
+        locationCallback = object: LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                mLastLocation = p0!!.locations[p0?.locations.size-1] // Get last location
+
+                if(mMarker!= null) {
+                    mMarker?.remove()
+                }
+
+                latitude = mLastLocation.latitude
+                longitude = mLastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions()
+                        .position(latLng)
+                        .title("Position")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                mMarker = mMap?.addMarker(markerOptions)
+
+                mMap?.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                mMap?.animateCamera(CameraUpdateFactory.zoomTo(11f))
+            }
+        }
+    }
+
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
+
+    private fun checkLocationPermission() : Boolean{
+        return if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_CODE)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), MY_PERMISSION_CODE)
+            }
+            false
+        } else {
+            true
+        }
+    }
+
+    // Override onRequestPermissions
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            MY_PERMISSION_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        if(checkLocationPermission()) {
+                            mMap?.isMyLocationEnabled = true
+                        }
+                    } else {
+                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -35,9 +136,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Init google play service
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap?.isMyLocationEnabled = true
+            }
+        } else {
+            mMap?.isMyLocationEnabled = true
+        }
+
+        // Enable zoom control
+        mMap.uiSettings.isZoomControlsEnabled = true
+
     }
+
 }
