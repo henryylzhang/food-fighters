a42399c83903a39dd312e9fa09da506cfd685398
Henry Zhang <henry.zhang@live.com>
2018-06-06 00:54:56 -0700

    Add Yelp API call functionality

--- app/src/main/java/edu/washington/zhang007/foodfighters/LandingActivity.kt
+++ app/src/main/java/edu/washington/zhang007/foodfighters/LandingActivity.kt
@@ -19,32 +19,30 @@ class LandingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_landing)
 
         val apiFactory = YelpFusionApiFactory()
-        val yelpFusionApi = apiFactory.createAPI(R.string.yelp_key.toString())
-
+        val yelpFusionApi = apiFactory.createAPI(getString(R.string.yelp_key))
         val params: HashMap<String, String> = hashMapOf()
 
-
         params.put("latitude", "47.6553")
-        params.put("longitude", "122.3035")
-        params.put("radius", "4000")
-        params.put("limit", "5")
-
+        params.put("longitude", "-122.3035")
+        params.put("radius", "1000")
+        params.put("limit", "3")
 
         val callback = object : Callback<SearchResponse> {
             override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                 val searchResponse = response.body()
 
                 val totalItems = searchResponse.total
+                Log.i(TAG, totalItems.toString())
 
                 val businesses = searchResponse.businesses
 
-                for (i in 0..totalItems) {
-                    Log.i(TAG, businesses.get(i).name)
+                for (i in 0..(totalItems - 1)) {
+                    Log.i(TAG, i.toString() + " = " + businesses.get(i).name)
                 }
             }
 
             override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                Log.e(TAG, "Something terrible has happened")
+                Log.e(TAG, t.toString())
             }
         }
 
