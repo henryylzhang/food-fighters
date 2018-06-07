8f46a0f283564cf66a7b6b6546cb1512c0060999
Henry Zhang <henry.zhang@live.com>
2018-06-06 20:39:03 -0700

    More Yelp stuff

--- app/src/main/java/edu/washington/zhang007/foodfighters/LandingActivity.kt
+++ app/src/main/java/edu/washington/zhang007/foodfighters/LandingActivity.kt
@@ -8,6 +8,7 @@ import com.yelp.fusion.client.models.SearchResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.Random
 
 
 class LandingActivity : AppCompatActivity() {
@@ -22,22 +23,24 @@ class LandingActivity : AppCompatActivity() {
         val yelpFusionApi = apiFactory.createAPI(getString(R.string.yelp_key))
         val params: HashMap<String, String> = hashMapOf()
 
-        params.put("latitude", "47.6553")
-        params.put("longitude", "-122.3035")
-        params.put("radius", "1000")
-        params.put("limit", "3")
+        params.put("latitude", "47.6553") // will be user set
+        params.put("longitude", "-122.3035") // will be user set
+        params.put("radius", "1000") // will be user set
+        params.put("limit", "50") // set to maximum per call
 
         val callback = object : Callback<SearchResponse> {
+
+
             override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                 val searchResponse = response.body()
 
-                val totalItems = searchResponse.total
-                Log.i(TAG, totalItems.toString())
+                val restaurantLimit = 5 // this will be set to user preference later on
 
                 val businesses = searchResponse.businesses
 
-                for (i in 0..(totalItems - 1)) {
-                    Log.i(TAG, i.toString() + " = " + businesses.get(i).name)
+                for (i in 0..(restaurantLimit - 1)) {
+                    val j = (0..50).random() // upper limit should be as many as I can pull w/ offset?
+                    Log.i(TAG, i.toString() + " restnum=" + j.toString() + " = " + businesses.get(j).name)
                 }
             }
 
@@ -49,4 +52,7 @@ class LandingActivity : AppCompatActivity() {
         val call = yelpFusionApi.getBusinessSearch(params)
         call.enqueue(callback)
     }
+
+    fun ClosedRange<Int>.random() =
+            Random().nextInt(endInclusive - start) +  start
 }
