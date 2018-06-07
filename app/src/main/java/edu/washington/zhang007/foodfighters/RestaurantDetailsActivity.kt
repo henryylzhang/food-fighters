99cf76ea67ba6c884d1aae0170db1b28b18e8f2e
Henry Zhang <henry.zhang@live.com>
2018-06-07 11:19:35 -0700

    Add SMS invite funtionality

--- app/src/main/java/edu/washington/zhang007/foodfighters/RestaurantDetailsActivity.kt
+++ app/src/main/java/edu/washington/zhang007/foodfighters/RestaurantDetailsActivity.kt
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.telephony.SmsManager
 import android.widget.Toast
 import com.yelp.fusion.client.models.Business
 import kotlinx.android.synthetic.main.activity_restaurant_details.*
@@ -21,33 +22,55 @@ class RestaurantDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_details)
 
-        val restName = text_restaurantName
-        val rating = text_rating
-        val phone = text_phone
-        val hours = text_businessHours
+        // check for SMS permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.SEND_SMS),
+                    MY_PERMISSIONS_REQUEST_SEND_SMS)
+        }
 
-        val business = intent.getSerializableExtra("Business") as Business
+            val restName = text_restaurantName
+            val rating = text_rating
+            val phone = text_phone
+            val hours = text_businessHours
 
-        if (business == null) { println("YO I'M NULL") }
-        restName.text = business.name
-        rating.text = business.rating.toString() + " Stars"
-        phone.text = business.phone
-        // hours.text = business.hours.toString()
+            val sendText = btn_text
 
-        phone.setOnClickListener {
-            val intent = Intent(Intent.ACTION_CALL)
-            intent.setData(Uri.parse("tel:" + phone.text))
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.CALL_PHONE),
-                        MY_PERMISSIONS_REQUEST_CALL_PHONE)
-                startActivity(intent)
-            } else {
-                startActivity(intent)
+            val business = intent.getSerializableExtra("Business") as Business
+
+            if (business == null) {
+                println("YO I'M NULL")
+            }
+            restName.text = business.name
+            rating.text = business.rating.toString() + " Stars"
+            phone.text = business.phone
+            // hours.text = business.hours.toString()
+
+            phone.setOnClickListener {
+                val intent = Intent(Intent.ACTION_CALL)
+                intent.setData(Uri.parse("tel:" + phone.text))
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.CALL_PHONE),
+                            MY_PERMISSIONS_REQUEST_CALL_PHONE)
+                    startActivity(intent)
+                } else {
+                    startActivity(intent)
+                }
+            }
+
+            btn_text.setOnClickListener {
+                val sms = SmsManager.getDefault()
+
+                val phoneNumber = phone.text.toString()
+                val message = "Join me at " + restName.text.toString() + "! at " +
+                business.coordinates.latitude.toString() + business.coordinates.longitude.toString()
+
+                sms.sendTextMessage(phoneNumber, null, message, null, null)
             }
         }
-    }
 
     override fun onRequestPermissionsResult(requestCode: Int,
                                             permissions: Array<String>, grantResults: IntArray) {
